d30e23edf82e69e41ed3fdf29c6eef56b4c461f0
BTV Sumanth <76842817+sumanth2002629@users.noreply.github.com>
2023-12-12 19:05:07 +0530

    Update teardown.js

--- backend/tests/teardown.js
+++ backend/tests/teardown.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-    process.exit(0)
+const globalTeardown = async () => {
+  // Check if any test failed and throw an error if so
+  if (global.testResults.numFailedTests > 0) {
+    throw new Error('Not all tests passed!');
   }
+  else{
+    process.exit(0);
+  }
+};
+
+module.exports = { globalTeardown };
